4ad258cf29053adc9e9a920cb53f449459af6d03
radkum <radoslaw.kumorekit@gmail.com>
2024-09-23 09:23:19 +0200

    build elam with makefile

--- build.rs
+++ build.rs
@@ -2,7 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     wdk_build::configure_wdk_binary_build()?;
 
     let resource_name = std::env::var("CARGO_PKG_NAME")?.replace("-", "_");
-    //let resource_name = resource_name.strip_suffix("_rs")?;
     println!("cargo:rerun-if-changed={resource_name}");
     embed_resource::compile(resource_name, embed_resource::NONE);
     Ok(())
